8d839e28ff8ec592bcc0bc98f49fb5f2df3a89fe
Craig Ambrose <craig@craigambrose.com>
2023-10-07 14:12:10 -0400

    don't try and set ssh key for now

--- provider.tf
+++ provider.tf
@@ -8,12 +8,8 @@ terraform {
 }
 
 variable "do_token" {}
-variable "pvt_key" {}
+# variable "pvt_key" {}
 
 provider "digitalocean" {
   token = var.do_token
-}
-
-data "digitalocean_ssh_key" "terraform" {
-  name = "terraform"
 }
