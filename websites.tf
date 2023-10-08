655494f6b9132def3c8a1b12f8dc0fbc71ccdf1d
Craig Ambrose <craig@craigambrose.com>
2023-10-08 18:50:29 -0400

    added base level domain

--- websites.tf
+++ websites.tf
@@ -16,9 +16,37 @@ resource "digitalocean_reserved_ip" "websites" {
 
 resource "digitalocean_record" "www" {
   domain = digitalocean_domain.default.id
-  type   = "A"
+  type   = "CNAME"
   name   = "www"
-  value  = digitalocean_reserved_ip.websites.ip_address
+  value  = "merri-bek-tech.github.io."
+}
+
+resource "digitalocean_record" "base-1" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.108.153"
+}
+
+resource "digitalocean_record" "base-2" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.109.153"
+}
+
+resource "digitalocean_record" "base-3" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.110.153"
+}
+
+resource "digitalocean_record" "base-4" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.111.153"
 }
 
 resource "digitalocean_record" "websites" {
