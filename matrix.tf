5f5b4997e9800ca5f8fa8f1ac5456012a8dbb757
Craig Ambrose <craig@craigambrose.com>
2023-10-18 18:08:25 +1100

    added labs

--- matrix.tf
+++ matrix.tf
@@ -1,36 +1,36 @@
-resource "digitalocean_droplet" "matrix-1" {
+resource "digitalocean_droplet" "labs-matrix-1" {
     image = "ubuntu-23-04-x64"
     name = "matrix-1"
     region = "syd1"
-    size = "s-1vcpu-512mb-10gb"
+    size = "s-1vcpu-1gb"
     backups = true
     ssh_keys = [
       data.digitalocean_ssh_key.root.fingerprint
     ]
 }
 
-resource "digitalocean_reserved_ip" "matrix" {
-  droplet_id = digitalocean_droplet.matrix-1.id
-  region     = digitalocean_droplet.matrix-1.region
+resource "digitalocean_reserved_ip" "labs-matrix" {
+  droplet_id = digitalocean_droplet.labs-matrix-1.id
+  region     = digitalocean_droplet.labs-matrix-1.region
 }
 
-resource "digitalocean_record" "matrix" {
+resource "digitalocean_record" "labs-matrix" {
   domain = digitalocean_domain.chat.id
   type   = "A"
-  name   = "@"
-  value  = digitalocean_reserved_ip.matrix.ip_address
+  name   = "labs"
+  value  = digitalocean_reserved_ip.labs-matrix.ip_address
 }
 
-resource "digitalocean_record" "matrix-server" {
+resource "digitalocean_record" "labs-matrix-server" {
   domain = digitalocean_domain.chat.id
   type   = "A"
-  name   = "matrix"
-  value  = digitalocean_reserved_ip.matrix.ip_address
+  name   = "matrix.labs"
+  value  = digitalocean_reserved_ip.labs-matrix.ip_address
 }
 
-resource "digitalocean_project_resources" "matrix-resources" {
-  project = digitalocean_project.default.id
+resource "digitalocean_project_resources" "labs-matrix-resources" {
+  project = digitalocean_project.labs.id
   resources = [
-    digitalocean_droplet.matrix-1.urn
+    digitalocean_droplet.labs-matrix-1.urn
   ]
 }
--- project.tf
+++ project.tf
@@ -1,6 +1,13 @@
 resource "digitalocean_project" "default" {
-  name        = "merri-bek-tech"
+  name        = "merri-bek-tech-prod"
   description = "Merri-bek Tech's live production hosting project."
   purpose     = "Web Application"
   environment = "Production"
+}
+
+resource "digitalocean_project" "labs" {
+  name        = "merri-bek-tech-labs"
+  description = "Merri-bek Tech's labs hosting project."
+  purpose     = "Web Application"
+  environment = "Staging"
 }
